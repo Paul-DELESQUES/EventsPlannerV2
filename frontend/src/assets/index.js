25dd69b0d7d9202f1cb4bb74313923d94a1493be
Paul-DELESQUES <pldelesques+github@gmail.com>
2024-02-14 15:20:01 +0100

    improving the scss

--- frontend/src/assets/index.js
+++ frontend/src/assets/index.js
@@ -2,7 +2,7 @@ import bgKanban from "./bg_kanban.jpg";
 import customers from "./customers.svg";
 import dashboard from "./dashboard.svg";
 import event from "./event.svg";
-import logo from "./logo.svg";
+import logo from "./logo.png";
 import menu from "./menu.svg";
 import setting from "./setting.svg";
 import tasksBoard from "./tasksBoard.svg";
